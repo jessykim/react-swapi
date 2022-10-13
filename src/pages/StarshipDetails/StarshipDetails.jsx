fe74cff9cf9a2380953152e5fc467a7ffbb4d77f
jessikim <jessica.kim17@gmail.com>
2022-10-12 19:13:50 -0500

    render starship name on details page

--- src/pages/StarshipDetails/StarshipDetails.jsx
+++ src/pages/StarshipDetails/StarshipDetails.jsx
@@ -1,24 +1,24 @@
-// import { useState, useEffect } from 'react'
-// import { useLocation } from 'react-router-dom'
-
-
+import { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import { getDetails } from '../../services/sw-api'
 
 const StarshipDetails = () => {
-  // const [starshipDetails, setStarshipDetails] = useState({})
-  // const location = useLocation()
+  const [starshipDetails, setStarshipDetails] = useState({})
+  const location = useLocation()
 
-  // useEffect(() => {
-  //   const fetchDetails = async () => {
-  //     const starshipDetails = await getDetails(location.state.starship.url)
-  //     setStarshipDetails(starshipDetails)
-  //   }
-  //   fetchDetails()
-  // }, [location.state.starship.url])
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const starshipDetails = await getDetails(location.state.starship.url)
+      setStarshipDetails(starshipDetails)
+    }
+    fetchDetails()
+  }, [location.state.starship.url])
 
   return ( 
     <>
       <div>
-        Starship Details
+        <h2>Starship Details</h2>
+        <h4>{starshipDetails.name}</h4>
       </div>
     </> 
   );
