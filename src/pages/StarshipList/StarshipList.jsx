ee1d06a25d914402e3cc5fec63910dbbc5471383
jessikim <jessica.kim17@gmail.com>
2022-10-13 17:23:37 -0500

    style starship list page

--- src/pages/StarshipList/StarshipList.jsx
+++ src/pages/StarshipList/StarshipList.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { getAllStarships } from '../../services/sw-api'
 import { Link } from 'react-router-dom'
+import './StarshipList.css'
 
 const StarshipList = () => {
   const [starships, setStarships] = useState([])
@@ -15,19 +16,24 @@ const StarshipList = () => {
 
   return ( 
     <>
-      <h3>STAR WARS STARSHIPS</h3>
-      <div className='ship-container'>
-        {starships.map(starship => 
-          <Link
-            key={starship.name}
-            to="/starship"
-            state={{ starship }}
-          >
-            <div key={starship.name}>
-              {starship.name} 
-            </div>
-          </Link> 
-        )}
+      <div className='page'>
+        <div id='title'>
+          <h3>STAR WARS STARSHIPS</h3>
+        </div>
+        <div className='all-ships'>
+          {starships.map(starship => 
+              <Link
+                key={starship.name}
+                to="/starship"
+                state={{ starship }}
+                className="ship-container"
+              >
+                <div key={starship.name}>
+                  {starship.name} 
+                </div>
+              </Link> 
+          )}
+        </div>
       </div>
     </> 
   );
